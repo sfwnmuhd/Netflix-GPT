cacfd010642c9f4cfe0a3e78a310f248518b3ec6
Muhammad Safwan P K <62184316+sfwnmuhd@users.noreply.github.com>
2025-04-06 18:49:00 +0530

    netflixgpt basic folder structure created

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const AppLayout = () =>{
+    return (
+        <h1>hello world</h1>
+    )
+} 
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<AppLayout />);
